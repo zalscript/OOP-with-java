151990c9085c165ff892004e3e1ec8f9c3df04a5
isaaal <faizalrahmancontact@gmail.com>
2025-06-13 17:18:16 +0700

    Masih Eror

--- CaseExceptionHandling/src/Exception.java
+++ CaseExceptionHandling/src/Exception.java
@@ -1,7 +1,18 @@
 public class Exception {
 
+//     public static void main(String[] args) {
+//         int a[] = new int[5];
+//         a[5] = 100;
+//     }
+// }
     public static void main(String[] args) {
         int a[] = new int[5];
-        a[5] = 100;
-    }
+        try
+        {
+            a[5] = 100;
+        }
+        catch (Exception e) {
+        }
+            System.out.println("Terjadi pelanggaran memory");
+    } 
 }
